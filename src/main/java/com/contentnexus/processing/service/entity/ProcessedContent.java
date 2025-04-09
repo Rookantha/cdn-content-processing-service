14151af1661d4f1d32e070ee0a1d868b8d89249c
rookantha <rookantha.vc@gmail.com>
2025-04-10 03:40:56 +0530

    google auth and yml configuration

--- src/main/java/com/contentnexus/processing/service/entity/ProcessedContent.java
+++ src/main/java/com/contentnexus/processing/service/entity/ProcessedContent.java
@@ -2,39 +2,26 @@ package com.contentnexus.processing.service.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-@Entity
 @Data
 @NoArgsConstructor
-@Table(name = "processedContents")
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Document(collection = "processedContents")
 public class ProcessedContent {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String id;  // MongoDB uses String IDs by default
 
-    @Column(nullable = false, unique = true)
     private String videoId;
-
-    @Column(nullable = false)
     private String rawVideoPath;
-
-    @Column(nullable = true)
     private String templateId;
-
-    @Column(nullable = true)
     private String processedVideoPath;
-
-    @Column(nullable = true)
     private String status;
-
-    @Column(nullable = true)
     private String encodingDetails;
-
-    @Column(nullable = true)
     private String errorDetails;
 }
--- src/main/java/com/contentnexus/processing/service/repository/ProcessedContentRepository.java
+++ src/main/java/com/contentnexus/processing/service/repository/ProcessedContentRepository.java
@@ -1,7 +1,7 @@
 package com.contentnexus.processing.service.repository;
 
 import com.contentnexus.processing.service.entity.ProcessedContent;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ProcessedContentRepository extends JpaRepository<ProcessedContent, Long> {
+public interface ProcessedContentRepository extends MongoRepository<ProcessedContent, Long> {
 }
